6f9d195a707a0764a753cfe7cd680a2d24070270
wavey <>
2025-05-18 17:57:24 +0100

    added setup

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export XDG_CONFIG_HOME="$HOME"/.config
+mkdir -p "$XDG_CONFIG_HOME"
+
+ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
+
+packages=(
+  fd
+  ripgrep
+  npm
+  lazygit
+)
+
+for package in "${packages[@]}"; do
+  /home/linuxbrew/.linuxbrew/bin/brew install "$package"
+done
