85adc0b4d7cb70a81cc80b5276dc589838cc3093
wavey <>
2025-05-18 18:28:13 +0100

    removing brew packages

--- setup.sh
+++ setup.sh
@@ -4,17 +4,3 @@ export XDG_CONFIG_HOME="$HOME"/.config
 mkdir -p "$XDG_CONFIG_HOME"
 
 ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
-
-packages=(
-  fd
-  ripgrep
-  npm
-  lazygit
-)
-
-ls -la /home
-ls -la /home/linuxbrew
-
-for package in "${packages[@]}"; do
-  /home/linuxbrew/.linuxbrew/bin/brew install "$package"
-done
